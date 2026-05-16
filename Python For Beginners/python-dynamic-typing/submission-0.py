cc81fe9b66239024d3f4430352b36b27d04f58d2
Gabriel Sgrâncio Melo Correia <biel.sgrancio@hotmail.com>
2026-05-16 15:30:21 -0300

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,11 @@
+
+variable = 10
+print(type(variable))
+variable = 10.20
+print(type(variable))
+variable = True
+print(type(variable))
+variable = 'true'
+print(type(variable))
+variable = []
+print(type(variable))
