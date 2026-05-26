d241ffa6313277de7996c3b5256f34dd7f4a63e4
Gabriel Sgrâncio Melo Correia <biel.sgrancio@hotmail.com>
2026-05-26 12:49:56 -0300

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,13 @@
+def add_two_numbers() -> int:
+    n = input()
+    n_ = n.split(",")
+
+    return int(n_[0]) + int(n_[1])
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
